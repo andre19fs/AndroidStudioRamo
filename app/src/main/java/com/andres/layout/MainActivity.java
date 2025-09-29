0040f1df1361647c995ed23d09b6ecf83fcd1632
andre19fs <andres.pasten2005@gmail.com>
2025-09-29 20:58:28 -0300

    Funcionalidad cargar imagen en hilo secundario

--- app/src/main/java/com/andres/layout/MainActivity.java
+++ app/src/main/java/com/andres/layout/MainActivity.java
@@ -1,6 +1,10 @@
 package com.andres.layout;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    Button btnCargar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        imageView = findViewById(R.id.imageView);
+        btnCargar = findViewById(R.id.btnCargar);
+        btnCargar.setOnClickListener((view) -> {
+            cargarImagenEnHiloSecundario(btnCargar);
+        });
+    }
+
+    private Bitmap loadImageFromNetwork(String urlString) {
+        try {
+            java.net.URL url = new java.net.URL(urlString);
+            java.io.InputStream input = url.openStream();
+            return android.graphics.BitmapFactory.decodeStream(input);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public void cargarImagenEnHiloSecundario(View view) {
+        new Thread(() -> {
+            final Bitmap bitmap = loadImageFromNetwork("https://www.advantour.com/img/japan/images/index.jpg");
+            imageView.post(() -> {
+                imageView.setImageBitmap(bitmap);
+            });
+        }).start();
     }
 }
